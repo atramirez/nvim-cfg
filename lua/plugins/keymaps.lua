b76165148e1545569f727719a5a5e638108166a2
atramirez <adenramirez166@gmail.com>
2025-02-23 14:51:29 -0700

    add keybinds

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<leader>ff', ":Telescope find_files<cr>")
 vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<cr>")
 vim.keymap.set('n', '<leader>fb', ":Telescope buffers<cr>")
 vim.keymap.set('n', '<leader>fh', ":Telescope oldfiles<cr>")
+vim.keymap.set('n', '<leader>ft', ":Telescope treesitter<cr>")
+vim.keymap.set('n', '<leader>fl', ":Telescope current_buffer_fuzzy_find<cr>")
+
